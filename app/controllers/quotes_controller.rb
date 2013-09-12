f463cf45a8c4b9b7fefde6a800919908cb8a7257
Michael van Holst <mikevanholst@gmail.com>
2013-09-12 18:02:44 -0400

    adds working controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,7 @@
-class QuotesController < Rulers::QuotesController
+class QuotesController < Rulers::Controller
   def a_quote
     "There is nothing either good or bad " +
       "but thinking makes it so"
   end
 end
-  
+
